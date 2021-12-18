9e26dc98932ed1d07dbb8064c3e9d33663564e11
kerperr <p.castrec@gmail.com>
2021-12-18 12:54:48 +0100

    template use

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+import fs from 'fs'
 import Vue from 'vue'
 import express from 'express'
 import { createRenderer } from 'vue-server-renderer'
@@ -5,7 +6,17 @@ import { createRenderer } from 'vue-server-renderer'
 const server = express()
 server.use(express.urlencoded({ extended: true }))
 server.use(express.json())
-const renderer = createRenderer()
+
+const template = fs.readFileSync('./templates/index.template.html', 'utf-8')
+const renderer = createRenderer({ template })
+
+const ctx = {
+    title: 'vue-ssr',
+    metas: `
+        <meta name="keyboard" content="vue, ssr">
+        <meta name="description" content="vue ssr demo">
+    `
+}
 
 server.get('*', (req, res) => {
 
@@ -14,15 +25,15 @@ server.get('*', (req, res) => {
         template: `<div>You are on {{ url }}</div>`
     })
 
-    renderer.renderToString(app)
-        .then(html => res.end(`
-    <!DOCTYPE html>
-    <html lang="en">
-      <head><title>Hello</title></head>
-      <body>${html}</body>
-    </html>
-  `))
-        .catch(err => res.status(500).end('Internal Server Error'))
+    renderer.renderToString(app, ctx)
+        .then(html => res.end(html))
+        .catch(err => {
+            console.log(err)
+            res.status(500).end('Internal Server Error')
+        })
 })
 
-server.listen(8080, () => console.log('http://localhost:8080'))
+server.listen(
+    8080,
+    () => console.log('http://localhost:8080')
+)
